55bc666997067b77d21ed0cf5d10c680a943999a
vimboom <leevimboom@gmail.com>
2025-12-18 13:27:50 +0800

    Update project files

--- convolution_RX.m
+++ convolution_RX.m
@@ -1,24 +1,30 @@
-%[text] %[text:anchor:T_01D54C26] # Convolution (and downsampling)
-%[text] This function implements a disrete convolution between a waveform and the impulse response of the RX filter. The resulting vector is then cropped and downsampled in order to retrieve the RX symbol vector.
-%[text] <u>**Inputs:**</u>
-%[text] - *signal*: signal vector of size *\[ ( nb\_symb - 1 ) \* sps + length( g ) \] x 1*
-%[text] - *g*: Impulse Response (IR) of the pulse of size *1 x* length( *g* )
-%[text] - *sps*: number of samples per symbol  \
-%[text] <u>**Ouput:**</u>
-%[text] - *symb\_RX*: filtered signal which corresponds to received symbols (column vector) \
 function symb_RX = convolution_RX( signal , g , sps )
-%[text:tableOfContents]{"heading":"**Table of Contents**"}
-%[text] %[text:anchor:H_034D8590] ## Discrete Convolution
-%[text] This part is simliar to the "Discrete Convolution" section of the function "convolution\_TX" and can therefore be directly copy-pasted (only variable names need to be adjusted)
+%% CONVOLUTION_RX Convolution (and downsampling)
+% This function implements a disrete convolution between a waveform and the 
+% impulse response of the RX filter. The resulting vector is then cropped and 
+% downsampled in order to retrieve the RX symbol vector.
+% 
+% *Inputs:*
+%% 
+% * _signal_: signal vector of size _[ ( nb_symb - 1 ) * sps + length( g ) ] 
+% x 1_
+% * _g_: Impulse Response (IR) of the pulse of size _1 x_ length( _g_ )
+% * _sps_: number of samples per symbol 
+%% 
+% *Ouput:*
+%% 
+% * _symb_RX_: filtered signal which corresponds to received symbols (column 
+% vector)
+%% Discrete Convolution
+% This part is simliar to the "Discrete Convolution" section of the function 
+% "convolution_TX" and can therefore be directly copy-pasted (only variable names 
+% need to be adjusted)
 % Code the discrete convolution here
 Lg = length(g);
 L = (Lg-1)/2;
 nb_sample = length(signal);
-
 UPsymb_RX_filled = [zeros(2*L,1); signal; zeros(2*L,1)];
-
 conv_sig_RX = zeros(nb_sample+4*L,1);
-
 for i=L+1:nb_sample+3*L
     tp_sum = 0;
     for j=-L:L
@@ -26,21 +32,20 @@ for i=L+1:nb_sample+3*L
     end
     conv_sig_RX(i) = tp_sum;
 end
-%[text] %[text:anchor:H_91CC9298] ### Vector cropping
-%[text] This part is simliar to the "Discrete Convolution" section of the function "convolution\_TX" and can therefore be directly copy-pasted (only variable names need to be adjusted).
+% Vector cropping
+% This part is simliar to the "Discrete Convolution" section of the function 
+% "convolution_TX" and can therefore be directly copy-pasted (only variable names 
+% need to be adjusted).
 % Crop the resulting vector here
 UPsymb_RX = conv_sig_RX(L+1:nb_sample+3*L);
-%[text] %[text:anchor:H_D7D82C77] ## Downsampling
-%[text] In order to retrieve the symbols at RX, the received filtered signal needs to be sampled at the sampling rate (every *pT* in slides 21-24 of the course 2). This corresponds to downsample the filtered signal by a factor *sps*.
+%% Downsampling
+% In order to retrieve the symbols at RX, the received filtered signal needs 
+% to be sampled at the sampling rate (every _pT_ in slides 21-24 of the course 
+% 2). This corresponds to downsample the filtered signal by a factor _sps_.
 % Code the downsampling here
 nb_symb = floor((nb_sample+2*L-1)/sps) +1;
-
 symb_RX = zeros(nb_symb,1);
 for i=1:nb_symb
     symb_RX(i) = UPsymb_RX((i-1)*sps + 1);
 end
-
 end
-
-%[appendix]{"version":"1.0"}
-%---
--- generate_channel.m
+++ generate_channel.m
@@ -1,50 +1,73 @@
-%[text] %[text:anchor:T_027FEE12] # Generation of the channel model
-%[text] This function implements the Rayleigh and Rice channel models. The details of such models can be found in the chapter 2 of the course of Philippe De Doncker (see especially section 2.2.2.).
-%[text] <u>**Inputs:**</u>
-%[text] - *nb\_realization*: indicates the number of realizations of the channel (scalar quantity)
-%[text] - *channel\_type*: indicates the type of channel to generate (string quantity). Can take 2 values: *Rayleigh, Rice*
-%[text] - *K:* Rice factor (scalar quantity) \
-%[text]  <u>**Ouput:**</u>
-%[text] - *channel*: channel response (vector of size *1 x nb\_realization*) \
 function H = generate_channel( nb_realization , channel_type , K )
+%% GENERATE_CHANNEL Generation of the channel model
+% This function implements the Rayleigh and Rice channel models. The details 
+% of such models can be found in the chapter 2 of the course of Philippe De Doncker 
+% (see especially section 2.2.2.).
+% 
+% *Inputs:*
+%% 
+% * _nb_realization_: indicates the number of realizations of the channel (scalar 
+% quantity)
+% * _channel_type_: indicates the type of channel to generate (string quantity). 
+% Can take 2 values: _Rayleigh, Rice_
+% * _K:_ Rice factor (scalar quantity)
+%% 
+% *Ouput:*
+%% 
+% * _channel_: channel response (vector of size _1 x nb_realization_)
 switch channel_type
-%[text:tableOfContents]{"heading":"**Table of Contents**"}
-%[text] %[text:anchor:H_0951CEE4] ## Rayleigh channel (*N*-waves model)
-%[text] In a rich multipath Non-Line-Of-Sight (NLOS) environment, the propagation channel can be well modeled by a random variable following a complex normal distribution such as:
-%[text] $h\_{Rayleigh}\\sim\\mathit{C} \\mathcal{N}(0,1)$ 
-%[text] In this case, the channel magnitude $|h\_{Rayleigh}|$has a Rayleigh distribution and the channel phase $\\angle h\_{Rayleigh}$ has a uniform distribution over $2\\pi$.
-%[text] In this code, the following normalization, $\\mathrm{E}\\left\[ |h\_{Rayleigh}|^2 \\right\]=1$, should be observed (i.e., the variance of both the real and imaginary parts of $h\_{Rayleigh}$ is $\\sigma^2=\\frac{1}{2}$).
+%% Rayleigh channel (_N_-waves model)
+% In a rich multipath Non-Line-Of-Sight (NLOS) environment, the propagation 
+% channel can be well modeled by a random variable following a complex normal 
+% distribution such as:
+% 
+% $$h_{Rayleigh}\sim\mathit{C} \mathcal{N}(0,1)$$ 
+% 
+% In this case, the channel magnitude $|h_{Rayleigh}|$has a Rayleigh distribution 
+% and the channel phase $\angle h_{Rayleigh}$ has a uniform distribution over 
+% $2\pi$.
+% 
+% In this code, the following normalization, $\mathrm{E}\left[ |h_{Rayleigh}|^2 
+% \right]=1$, should be observed (i.e., the variance of both the real and imaginary 
+% parts of $h_{Rayleigh}$ is $\sigma^2=\frac{1}{2}$).
     case 'Rayleigh'
         % Code the Rayleigh channel here
         H_Ray_mag = raylrnd(1,1,nb_realization);
         H_Ray_mag = H_Ray_mag / sqrt( (sum(H_Ray_mag.^2)/nb_realization) );
-
         H_Ray_ph = 2*pi*rand(1,nb_realization);
-
         H = H_Ray_mag .* exp(1i*H_Ray_ph);
-%[text] %[text:anchor:H_EFEF96B5] ## Rice channel (*1+N*-waves model)
-%[text] In a rich multipath environment, if one multipath components (MPC) dominates the other multipath, the propagation channel can be well modeled by:
-%[text] $h\_{Rice}=\\sqrt{\\frac{K}{1+K}}h\_{strong MPC}+\\sqrt{\\frac{1}{1+K}}h\_{Rayleigh}$
-%[text] where:
-%[text] - $h\_{Rayleigh}\\sim\\mathit{C} \\mathcal{N}(0,1)$ with the following normalization $\\mathrm{E}\\left\[ |h\_{Rayleigh}|^2 \\right\]=1$ (i.e., the variance of both the real and imaginary parts of $h\_{Rayleigh}$ is $\\sigma^2=\\frac{1}{2}$).
-%[text] - $h\_{strong MPC}=e^{j\\phi}$ is a deterministic magnitude and phase value of a strong MPC. Here the magnitude is 1 so that the amplitude of the strong MPC is completely characterized with the Rice factor K.
-%[text] - $K=\\frac{ |h\_{strong MPC}|^2 }{\\mathrm{E}\\left\[ |h\_{NLOS}|^2 \\right\]}$ is the Rice factor
-%[text] - $|h\_{Rice}|$has a Rice distribution. \
-%[text] Typcally, the strong MPC can correspond to the Line-Of-Sight (LOS) contribution or to a strong reflection on a large wall for instance, whereas the second term $h\_{Rayleigh}$ corresponds to the NLOS contributions.
-%[text] **Tip:** You can put any specific value for $\\phi$ and observe the influence on the results in the main.
+%% Rice channel (_1+N_-waves model)
+% In a rich multipath environment, if one multipath components (MPC) dominates 
+% the other multipath, the propagation channel can be well modeled by:
+% 
+% $$h_{Rice}=\sqrt{\frac{K}{1+K}}h_{strong MPC}+\sqrt{\frac{1}{1+K}}h_{Rayleigh}$$
+% 
+% where:
+%% 
+% * $h_{Rayleigh}\sim\mathit{C} \mathcal{N}(0,1)$ with the following normalization 
+% $\mathrm{E}\left[ |h_{Rayleigh}|^2 \right]=1$ (i.e., the variance of both the 
+% real and imaginary parts of $h_{Rayleigh}$ is $\sigma^2=\frac{1}{2}$).
+% * $h_{strong MPC}=e^{j\phi}$ is a deterministic magnitude and phase value 
+% of a strong MPC. Here the magnitude is 1 so that the amplitude of the strong 
+% MPC is completely characterized with the Rice factor K.
+% * $K=\frac{ |h_{strong MPC}|^2 }{\mathrm{E}\left[ |h_{NLOS}|^2 \right]}$ is 
+% the Rice factor
+% * $|h_{Rice}|$has a Rice distribution.
+%% 
+% Typcally, the strong MPC can correspond to the Line-Of-Sight (LOS) contribution 
+% or to a strong reflection on a large wall for instance, whereas the second term 
+% $h_{Rayleigh}$ corresponds to the NLOS contributions.
+% 
+% *Tip:* You can put any specific value for $\phi$ and observe the influence 
+% on the results in the main.
     case 'Rice'
         % Code the Rice channel here
         H_Ray_mag = raylrnd(1,1,nb_realization);
         H_Ray_mag = H_Ray_mag / sqrt( (sum(H_Ray_mag.^2)/nb_realization) );
-
         H_Ray_ph = 2*pi*rand(1,nb_realization);
         H_Ray = H_Ray_mag .* exp(1i*H_Ray_ph);
-
         phy = 1;
         H_MPC = ones(1,nb_realization)*1*exp(1i*phy);
         
         H = sqrt(K/(1+K)).*H_MPC + sqrt(1/(1+K)).*H_Ray;
 end
-
-%[appendix]{"version":"1.0"}
-%---
